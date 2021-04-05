a89a92a455037dcd44b2b44c10eb30dd02c030e2
Patrick Lai <patricklai14@gmail.com>
2021-04-04 21:04:22 -0500

    changes to bp, lmdb examples

--- examples/example_bp.py
+++ examples/example_bp.py
@@ -11,52 +11,6 @@ from ase.calculators.emt import EMT
 from model_eval import model_evaluation
 
 def main():
-    #create configs
-    Gs_1 = {"default": {
-                "G2": {
-                    "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4).tolist(),
-                    "rs_s": [0]},
-                "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
-                "cutoff": 6,
-            }}
-
-    Gs_2 = copy.deepcopy(Gs_1)
-    Gs_2["default"]["cutoff"] = 2
-
-    config_1 = {"name": "test_job_1",
-                "fingerprint_type": "gaussian",
-                "bp_params": Gs_1,
-                "evaluation_type": "k_fold_cv",
-                "num_folds": 2,
-                "cv_iters": 2,
-                "nn_layers": 3,
-                "nn_nodes": 20,
-                "nn_learning_rate": 1e-3,
-                "nn_batch_size": 32,
-                "nn_epochs": 1000,
-                "seed": 1}
-
-    config_2 = {"name": "test_job_2",
-                "fingerprint_type": "gaussian",
-                "bp_params": Gs_2,
-                "evaluation_type": "k_fold_cv",
-                "num_folds": 2,
-                "cv_iters": 2,
-                "nn_layers": 3,
-                "nn_nodes": 20,
-                "nn_learning_rate": 1e-3,
-                "nn_batch_size": 32,
-                "nn_epochs": 1000,
-                "seed": 1}
-
-    curr_dir = pathlib.Path(__file__).parent.absolute()
-
-    config_file_1 = curr_dir / "config_1.json" 
-    json.dump(config_1, open(config_file_1, "w+"), indent=2)
-
-    config_file_2 = curr_dir / "config_2.json"
-    json.dump(config_2, open(config_file_2, "w+"), indent=2)
-
     #setup dataset
     np.random.seed(3)
     distances = np.linspace(2, 5, 100)
@@ -84,13 +38,62 @@ def main():
                       "Cu": os.path.join(dir_prefix, "config/MCSH_potential/Cu_totaldensity_5.g")}
     data = model_evaluation.dataset(elements, images, atom_gaussians=atom_gaussians)
 
+    #create configs
+    Gs_1 = {"default": {
+                "G2": {
+                    "etas": np.logspace(np.log10(0.05), np.log10(5.0), num=4).tolist(),
+                    "rs_s": [0]},
+                "G4": {"etas": [0.005], "zetas": [1.0, 4.0], "gammas": [1.0, -1.0]},
+                "cutoff": 6,
+            }}
+
+    Gs_2 = copy.deepcopy(Gs_1)
+    Gs_2["default"]["cutoff"] = 2
+
+    config_1 = {
+        "name": "test_job_1",
+        "evaluation_type": "k_fold_cv",
+        "cv_iters": 2,
+        "num_folds": 2,
+        "seed": 1,
+        "amptorch_config": {
+            "model": {
+                "get_forces": False, 
+                "num_layers": 3, 
+                "num_nodes": 20},
+            "optim": {
+                "device": "cpu",
+                "force_coefficient": 0.0,
+                "lr": 1e-3,
+                "batch_size": 32,
+                "epochs": 1000,
+            },
+            "dataset": {
+                #notice that we don't set the actual dataset here
+                "val_split": 0,
+                "elements": elements,
+                "fp_scheme": "gaussian",
+                "fp_params": Gs_1,
+                "save_fps": False,
+            },
+            "cmd": {
+                "debug": False,
+                "run_dir": "./",
+                "seed": 1,
+                "identifier": "test",
+                "verbose": False,
+                "logger": False,
+            }
+        }
+    }
+
+    config_2 = copy.deepcopy(config_1)
+    config_2["name"] = "test_job_2"
+    config_2["amptorch_config"]["dataset"]["fp_params"] = Gs_2
+
     #run model evaluation
+    curr_dir = pathlib.Path(__file__).parent.absolute()
     workspace = curr_dir / "test_workspace"
-    
-    #running model evaluation by passing in a list of config files
-    #results = model_evaluation.evaluate_models(data, config_dicts=[str(config_file_1), str(config_file_2)],
-    #                                           enable_parallel=True, workspace=workspace,
-    #                                           time_limit="00:30:00", mem_limit=2, conda_env="amptorch")
 
     #running model evaluation by passing in a list of config dicts
     #replace "amptorch" below with name of conda env that has AMPTorch installed
